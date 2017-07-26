9316a3bd3e64a1ef648b2d19567a119b6d7adf65
Daniel <daniel.bunting@crick.ac.uk>
2017-07-26 11:47:53 +0000

    Fix typo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "RNA2CN",
+    version = '0.0.0',
+    author = "Daniel Bunting",
+    author_email = "daniel.bunting@crick.ac.uk",
+    url = "https://github.com/dnlbunting/rna2cn/",
+    packages=find_packages(),
+
+)
