a3e47f500b1e1c6c780858d4e7c92a979895f59c
Daniel Bunting <daniel.bunting@crick.ac.uk>
2017-08-23 14:00:16 +0100

    Add description

--- rna2cn/rna2cn_run.py
+++ rna2cn/rna2cn_run.py
@@ -9,7 +9,19 @@ os.environ['THEANO_FLAGS'] = "openmp=False"
 
 
 def main():
-    main_parser = argparse.ArgumentParser(prog='RNA2CN')
+    main_parser = argparse.ArgumentParser(prog='RNA2CN',
+                                          formatter_class=argparse.RawTextHelpFormatter,
+                                          description='''RNA2CN toolkit for predicting copy number aberrations from scRNAseq.
+
+Subcommands
+------------
+
+preprocess - Encodes input expression data and optionally target CNA profiles into features for model training/evaluation
+concatenate - Combine together multiple datasets separately prepared using the preprocess command
+train - Trains a LSTM neural network defined in Keras JSON format using the prepared input data
+evaluate - Evaluates the performance of the trained network on the test data and produces output graphs.
+
+''')
     main_parser.add_argument('command', choices=['train', 'preprocess', 'predict', 'evaluate', 'concatenate'])
 
     main_args = main_parser.parse_args(sys.argv[1:2])
